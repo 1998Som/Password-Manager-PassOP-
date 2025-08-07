c6d8b6b87ab69ea3e1c18f16c4aef9c20ddfb741
1998Som <somesankarsantra199818@gmail.com>
2025-08-07 13:05:48 +0530

    Initial commit

---
+++ pass-op/src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './components/Navbar'
+import './App.css'
+import Manager from './components/Manager'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+     <div className="mt-10 sm:mt-10"></div>
+    <Manager />
+    <Footer />
+    
+    </>
+  )
+}
+
+export default App
